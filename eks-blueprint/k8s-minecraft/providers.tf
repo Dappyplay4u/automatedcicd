858959a9957a0482bfaeab968dc3706070e7c055
Dappyplay4u <jegede.oladapo@ymail.com>
2024-02-05 07:27:35 -0500

    more updates

--- eks-blueprint/k8s-minecraft/providers.tf
+++ eks-blueprint/k8s-minecraft/providers.tf
@@ -1,39 +1,26 @@
-#  This indicates which versions of Terraform and providers our project will use
+# Terraform version and required providers
 terraform {
   required_version = ">= 1.3.4"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
-    }
-    random = {
-      version = ">= 3"
-    }
+    aws        = ">= 5.0.0"
+    kubernetes = ">= 2.20.0"
+    helm       = ">= 2.9.0"
+    kubectl    = ">= 1.14"
+    random     = ">= 3"
   }
 }
 
+# AWS provider configuration
 provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = {
-      Name    = "k8s_Minecraft"
-      project = "eks_demo"
-    }
-  }
+  region = "us-east-1"
+  access_key = "AKIAYISMX2RDNWTQ2KSC"
+  secret_key = "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"
 }
+
+
+
+# Kubernetes provider configuration
 provider "kubernetes" {
   host                   = module.eks_cluster.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
@@ -45,6 +32,7 @@ provider "kubernetes" {
   }
 }
 
+# Helm provider configuration
 provider "helm" {
   kubernetes {
     host                   = module.eks_cluster.cluster_endpoint
@@ -58,6 +46,7 @@ provider "helm" {
   }
 }
 
+# kubectl provider configuration
 provider "kubectl" {
   apply_retry_count      = 10
   host                   = module.eks_cluster.cluster_endpoint
@@ -71,6 +60,7 @@ provider "kubectl" {
   }
 }
 
+# Data source for EKS cluster authentication
 data "aws_eks_cluster_auth" "this" {
   name = module.eks_cluster.cluster_name
 }
