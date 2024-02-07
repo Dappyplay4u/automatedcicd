8952ecc43c1b4b64dde8c69bf098e1b889ed4263
Dappyplay4u <jegede.oladapo@ymail.com>
2024-02-06 21:23:59 -0500

    new update

--- eks-blueprint/k8s-minecraft/eks-worker-nodes.tf
+++ eks-blueprint/k8s-minecraft/eks-worker-nodes.tf
@@ -4,23 +4,76 @@
 #  * EKS Node Group to launch worker nodes
 #
 
+# resource "aws_iam_role" "demo-node" {
+#   name = "minecraft-eks-demo-node"
+
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+# }
+
+# resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role       = aws_iam_role.demo-node.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role       = aws_iam_role.demo-node.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role       = aws_iam_role.demo-node.name
+# }
+
+# resource "aws_eks_node_group" "demo" {
+#   cluster_name    = aws_eks_cluster.demo.name
+#   node_group_name = "demo"
+#   node_role_arn   = aws_iam_role.demo-node.arn
+#   subnet_ids      = aws_subnet.demo[*].id
+#   disk_size = 10
+#   instance_types = ["t2.medium"]
+
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 2
+#     min_size     = 2
+#   }
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.demo-node-AmazonEKSWorkerNodePolicy,
+#     aws_iam_role_policy_attachment.demo-node-AmazonEKS_CNI_Policy,
+#     aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
+#   ]
+# }
+
 resource "aws_iam_role" "demo-node" {
   name = "minecraft-eks-demo-node"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
@@ -43,8 +96,8 @@ resource "aws_eks_node_group" "demo" {
   node_group_name = "demo"
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
-  disk_size = 10
-  instance_types = ["t2.medium"]
+  disk_size       = 10
+  instance_types  = ["t2.medium"]
 
   scaling_config {
     desired_size = 2
